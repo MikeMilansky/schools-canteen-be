e0908b8a456e550323e1856379a3c6a9cc8d94cb
Andrey Korolev <and.korolyov@gmail.com>
2019-08-17 19:34:50 +0300

    Update CORS.

--- src/SchoolCanteen.BE/SchoolCanteen.BE/App_Start/WebApiConfig.cs
+++ src/SchoolCanteen.BE/SchoolCanteen.BE/App_Start/WebApiConfig.cs
@@ -9,10 +9,11 @@ namespace SchoolCanteen.BE
 	{
 		public static void Register(HttpConfiguration config)
 		{
-			// Web API configuration and services
+            // Web API configuration and services
+            config.EnableCors();
 
-			// Web API routes
-			config.MapHttpAttributeRoutes();
+            // Web API routes
+            config.MapHttpAttributeRoutes();
 
 			config.Routes.MapHttpRoute(
 				name: "DefaultApi",
